88e3bb3b82fec112ea748fa005d993471cad791c
songzeng <songzeng62@gmail>
2020-04-08 10:39:51 +0800

    update: add base.ts

---
+++ section1/base.js
@@ -0,0 +1,10 @@
+// boolean
+var isDone = false;
+// number
+var decLiteral = 20;
+var hexLiteral = 0x14;
+var binaryLiteral = 20;
+var octalLieral = 20;
+// string
+var personName = 'bob';
+personName = 'smith';
---
+++ section1/base.ts
@@ -0,0 +1,12 @@
+// boolean
+const isDone: boolean = false;
+
+// number
+const decLiteral: number = 20;
+const hexLiteral: number = 0x14;
+const binaryLiteral: number = 0b10100;
+const octalLieral: number = 0o24;
+
+// string
+let personName: string = 'bob';
+personName = 'smith';
